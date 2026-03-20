697c2f21a7b32125269980dc648c27011aa673a8
write4ashok <86650978+write4ashok@users.noreply.github.com>
2026-03-20 15:14:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ provider "aws" {
   region = "ap-south-1"
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "app_server2" {
   ami           = data.aws_ami.ubuntu.id
   instance_type = "t3.micro"
   key_name      = "Devops"
